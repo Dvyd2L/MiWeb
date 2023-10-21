786f333bd184991d449822f942f90759d74f81f4
Dvyd <david.llopislaguna@gmail.com>
2023-10-21 19:30:31 +0200

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   .gitignore
    	modified:   css/style.css
    	new file:   css/variables.css
    	modified:   index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-alert('Aparece un mensaje');
+// variables
+const body = document.querySelector('body');
+
+// funcions
+const msg = (msg = 'Aparece un mensaje') => alert(msg);
+
+// events
+body.onload = (ev) => {
+  console.log(ev);
+  msg();
+};
